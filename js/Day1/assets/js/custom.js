536bdbf22ec60034d226f120f67348d83eab0cdf
bala <bala>
2026-06-01 17:02:07 +0530

    updated

---
+++ js/Day1/assets/js/custom.js
@@ -0,0 +1,139 @@
+// var,let,const
+// keyword, variable name - 
+// that using casing - 3 (camal,pascal , snake case)
+
+// help of  variable keyword - var,let,const.
+
+
+// let a ;
+// var b;
+
+
+// initialzation
+
+// var a  = 10
+// let b = 12
+
+
+
+// let a = 20 
+// let b = 10 
+
+// const add = ()=>{
+ 
+//     console.log(a+b);
+    
+
+
+// }
+
+// add()
+
+
+
+
+// let authveriable = "true"
+// console.log(authveriable);
+
+// let busTicket = 100
+// console.log(busTicket);
+
+
+// let beachSystem = "String process"
+
+// console.log(beachSystem);
+
+
+// let powerControl = 2000
+
+// console.log(powerControl);
+
+
+
+
+// 4 types of scoping
+
+// global
+// function
+
+// block
+// lexcial
+
+
+
+// Hoisting Methods for the process :
+
+
+
+
+
+
+
+
+
+
+
+
+//     if(true){
+
+//         var a = 10 
+          
+
+//     }
+    
+//   console.log('Function inside',a);
+    
+
+
+
+
+// console.log('Function inside',a);
+// add()
+
+// const add = ()=>{
+
+//    let a = 10
+
+//     const sub = ()=>{
+
+//         //console.log(a);
+
+//         return a+11
+        
+
+//     }
+
+
+// }
+
+// TDZ
+
+
+// console.log(a);
+
+// let a = 10;
+
+// let age = Number("25")
+
+// console.log(typeof age );
+
+
+
+// console.log(Boolean({}));
+// console.log("one"+"two");
+// let a = Number("one")
+
+// console.log(a);
+
+
+
+// console.log({}+{});
+
+// console.log(typeof null);
+
+
+
+// let a = 10
+// console.log(b);
+
+
