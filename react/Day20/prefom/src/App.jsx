20d2b37fda5bd2ada21700eec802b8c0d42de9d3
JananiS1418 <jananisomasundaram07@gmail.com>
2026-07-10 17:37:16 +0530

    updated

---
+++ react/Day20/prefom/src/App.jsx
@@ -0,0 +1,60 @@
+// import { useMemo, useState } from "react"
+
+
+// const App = () => {
+// console.log('Component ');
+
+//  const [price,setPrice] = useState(2)
+//  const [theme,setTheme] = useState(false)
+
+//  const total = useMemo(()=>{
+
+//    console.log('memo compluted value');
+   
+//    const  qnt = 10 * 10
+    
+//    return qnt
+
+//  },[]) 
+
+
+//  const themeChange = ()=>{
+//    console.log('Theme running');
+   
+//    setTheme(!theme)
+
+//  }
+
+
+//   return (
+//     <>
+//     <h1>
+//       {total}
+//     </h1>
+  
+//     <p>
+//       {theme?"Dark":"Light"}
+//     </p>
+// <button onClick={themeChange}>Change Theme</button>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+// const App = () => {
+//   return (
+//     <>
+    
+//     </>
+//   )
+// }
+
+// export default App
